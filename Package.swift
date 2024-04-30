9ac65feee440a654c197358902960a5994fc332d
xxbrendenxx <155623716+xxbrendenxx@users.noreply.github.com>
2024-04-30 17:55:19 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,84 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-web3modal",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "Web3Modal",
+            targets: ["Web3Modal"]
+        ),
+        .library(
+            name: "Web3ModalUI",
+            targets: ["Web3ModalUI"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/WalletConnect/WalletConnectSwiftV2",
+            .upToNextMinor(from: "1.13.0")
+        ),
+        .package(
+            url: "https://github.com/WalletConnect/QRCode",
+            .upToNextMinor(from: "14.3.1")
+        ),
+        .package(
+            url: "https://github.com/pointfreeco/swift-snapshot-testing",
+            .upToNextMinor(from: "1.10.0")
+        ),
+        .package(name: "CoinbaseWalletSDK", url: "https://github.com/WalletConnect/wallet-mobile-sdk", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "Web3Modal",
+            dependencies: [
+                "QRCode",
+                .product(
+                    name: "WalletConnect",
+                    package: "WalletConnectSwiftV2"
+                ),
+                "Web3ModalUI",
+                "Web3ModalBackport",
+                "CoinbaseWalletSDK"
+            ],
+            resources: [
+                .process("Resources/Assets.xcassets"),
+                .copy("PackageConfig.json")
+            ]
+        ),
+        .target(
+            name: "Web3ModalUI",
+            dependencies: [
+                "Web3ModalBackport"
+            ],
+            resources: [
+                .process("Resources/Assets.xcassets")
+            ]
+        ),
+        .target(
+            name: "Web3ModalBackport"
+        ),
+
+        // MARK: - Test Targets
+
+        .testTarget(
+            name: "Web3ModalTests",
+            dependencies: [
+                "Web3Modal",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ]
+        ),
+        .testTarget(
+            name: "Web3ModalUITests",
+            dependencies: [
+                "Web3ModalUI",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ]
+        )
+    ]
+)
